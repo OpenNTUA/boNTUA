b4d67715cd55db290e783a10e4a37732ea30b67f
luzede <85195923+luzede@users.noreply.github.com>
2022-12-04 12:04:12 +0000

    Nothing much

---
+++ src/events/interactionCreate.js
@@ -0,0 +1,21 @@
+const { Events } = require('discord.js');
+
+module.exports = {
+	name: Events.InteractionCreate,
+	execute: async (interaction) => {
+		if (!interaction.isChatInputCommand()) return;
+
+		const command = interaction.client.commands.get(interaction.commandName);
+
+		try {
+			await command.execute(interaction);
+		}
+		catch (error) {
+			console.error(error);
+			await interaction.editReply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+		console.log('---------');
+		console.log(interaction.user.username, 'triggered an interaction in');
+		console.log('---------');
+	},
+};
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 
-const { Client, Collection, GatewayIntentBits, Events } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { TOKEN } = require('../config.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
@@ -10,6 +10,7 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBit
 client.commands = new Collection();
 
 // __dirname is an environment variable that tells you the absolute path of the directory containing the currently executing file.
+// Commands
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(i => i.endsWith('.js'));
 
@@ -26,25 +27,22 @@ for (const file of commandFiles) {
 	}
 }
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+// Events
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter((e) => e.endsWith('.js'));
 
-client.on(Events.InteractionCreate, async (interaction) => {
-	if (!interaction.isChatInputCommand()) return;
+// Loading events
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	}
+	else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
-	const command = client.commands.get(interaction.commandName);
-	try {
-		await command.execute(interaction);
-	}
-	catch (error) {
-		console.error(error);
-		await interaction.editReply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-	console.log(interaction);
-});
 
 // Log in to Discord with your client's token
 client.login(TOKEN);
